7e62ea562bb19ec2a754b170a7822b3be7af31fd
um-saltanat <145360854+um-saltanat@users.noreply.github.com>
2023-11-17 14:24:45 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.calculator to javafx.fxml;
-    exports com.example.calculator;
-}
